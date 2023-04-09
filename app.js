eb6a8cfccb78376d0b8061f2a2b03ac7769e6441
Enes Uraz <axeprime91@gmail.com>
2023-04-09 09:05:31 +0300

    configure template engine and create home page

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+// static assets config
+app.use(express.static(path.join(__dirname, "assets")));
+
+// template engine config
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// render main page
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home" });
+});
 
 module.exports = app;
